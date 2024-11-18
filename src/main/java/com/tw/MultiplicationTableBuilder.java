b55da1b345b4098cadc24ea76f7ed184252fbb3f
CHENJO20 <CHENJO20@oocl.com>
2024-11-18 16:26:36 +0800

    feat: create isStartNoGreaterThanEnd function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -4,14 +4,21 @@ public class MultiplicationTableBuilder {
 
     public static void main(String[] args) {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
-        int start = 2;
+        int start = 5;
         int end = 4;
-        String multiplicationTable = builder.build(start, end);
-
-        System.out.println(multiplicationTable);
+        boolean isStartNoGreaterThanEnd = builder.isStartNoGreaterThanEnd(start, end);
+        System.out.println(isStartNoGreaterThanEnd);
+//        String multiplicationTable = builder.build(start, end);
+//
+//        System.out.println(multiplicationTable);
     }
 
     public String build(int start, int end) {
         return "";
     }
+
+    public static boolean isStartNoGreaterThanEnd(int start, int end){
+        return start <= end;
+    }
+
 }
