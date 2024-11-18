87e315f70f3dea591ffa291f14191c98edc3d1ad
CHENJO20 <CHENJO20@oocl.com>
2024-11-18 16:35:02 +0800

    feat: create isInputInRange function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -4,21 +4,29 @@ public class MultiplicationTableBuilder {
 
     public static void main(String[] args) {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
-        int start = 5;
+        int start = 1001;
         int end = 4;
         boolean isStartNoGreaterThanEnd = builder.isStartNoGreaterThanEnd(start, end);
-        System.out.println(isStartNoGreaterThanEnd);
+        boolean isInputInRange = isInputInRange(start, end);
+        System.out.println(isInputInRange);
+
+//        System.out.println(isStartNoGreaterThanEnd);
 //        String multiplicationTable = builder.build(start, end);
 //
 //        System.out.println(multiplicationTable);
+    }
+
+    public static boolean isInputInRange(int start, int end) {
+        return (start >= 1 && start <= 1000) && (end >= 1 && end <= 1000);
     }
 
     public String build(int start, int end) {
         return "";
     }
 
-    public static boolean isStartNoGreaterThanEnd(int start, int end){
+    public static boolean isStartNoGreaterThanEnd(int start, int end) {
         return start <= end;
     }
+
 
 }
